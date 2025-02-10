e6840f0b551d6773a91732d334c4fb3851ae0824
Diogo Nunes <d13nunes@gmail.com>
2025-02-10 12:26:12 +0000

    Initial Commit

---
+++ SoupLetter/SoupLetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoupLetterApp.swift
+//  SoupLetter
+//
+//  Created by Diogo Nunes on 10/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct SoupLetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SoupLetterTests/SoupLetterTests.swift
@@ -0,0 +1,16 @@
+//
+//  SoupLetterTests.swift
+//  SoupLetterTests
+//
+//  Created by Diogo Nunes on 10/02/2025.
+//
+
+import Testing
+
+struct SoupLetterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
