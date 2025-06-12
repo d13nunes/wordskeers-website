128276bb5a31febb6edf734c3828e77201fac257
Diogo Nunes <d13nunes@gmail.com>
2025-06-12 16:13:09 +0100

    imp: quote badge icon size

--- webapp/wordseekr/src/lib/daily-challenge/DailyQuoteTag.svelte
+++ webapp/wordseekr/src/lib/daily-challenge/DailyQuoteTag.svelte
@@ -9,7 +9,7 @@
 </script>
 
 <BaseTag {onclick}>
-	<div class="mt-0 flex h-4 w-4 flex-row gap-1 lg:h-8 lg:w-8">
+	<div class="mt-0 flex h-4 max-h-4 w-4 max-w-4 flex-row gap-1 lg:h-8 lg:w-8">
 		<DailyQuoteIcon />
 	</div>
 	<span class="text-black-500 text-sm font-medium lg:text-lg">Quotes</span>
